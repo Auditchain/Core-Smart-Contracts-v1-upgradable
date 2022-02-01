8f6d8eaad6085c7f1baee3a50367f580a53839a7
win2win <win2win@protonmail.com>
2022-02-01 16:51:41 -0600

    remove library

--- contracts/IAuditToken.sol
+++ contracts/IAuditToken.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
     
 interface IAuditToken {
     function mint(address to, uint256 amount) external returns (bool) ;
